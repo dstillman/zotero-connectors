ad017ace8a754ee7430ceece0241084d66e85275
Dan Stillman <dstillman@zotero.org>
2017-08-16 23:47:48 +0200

    XHR processDocuments()
    Use XMLHttpRequest with responseType: 'document' for same origin
    requests. For cross-origin requests, use responseType: 'text' from the
    background page and get a document from DOMParser in the content script.

--- src/common/test/tests/httpTest.js
+++ src/common/test/tests/httpTest.js
@@ -25,7 +25,7 @@
 
 describe("HTTP", function() {
 	var tab = new Tab();
-	let url = 'http://zotero-static.s3.amazonaws.com/test.html';
+	let url = 'https://zotero-static.s3.amazonaws.com/test.html';
 	
 	before(Promise.coroutine(function* () {
 		yield tab.init(url);
@@ -37,42 +37,36 @@ describe("HTTP", function() {
 	
 	describe("#processDocuments()", function() {
 		it('succeeds when loading a same-origin page', Promise.coroutine(function* () {
-			let url = 'http://zotero-static.s3.amazonaws.com/test.html?t';
+			let url = 'https://zotero-static.s3.amazonaws.com/test.html?t';
 			let [content, location] = yield tab.run(function(url) {
-				var deferred = Zotero.Promise.defer();
-				sinon.stub(Zotero.HTTP, 'isSameOrigin').returns(true);
-				Zotero.HTTP.processDocuments(url, function(doc) {
-					try {
-						let content = doc.querySelector('.csl-entry').innerText;
-						let location = doc.location.href;
-						deferred.resolve([content, location])
-					} catch (e) {
-						deferred.reject(e);
+				return Zotero.HTTP.processDocuments(
+					url,
+					function (doc) {
+						var content = doc.querySelector('.csl-entry').innerText;
+						var location = doc.location.href;
+						return [content, location];
 					}
-				});
-				
-				return deferred.promise.then(response => {Zotero.HTTP.isSameOrigin.restore(); return response});
+				)
+				// Get first element from all() promise
+				.then(vals => vals[0]);
 			}, url);
 			
 			assert.include(content, 'Rosenzweig');
 			assert.equal(location, url);
 		}));
+		
 		it('succeeds when loading a cross-origin page', Promise.coroutine(function* () {
-			let url = chrome.extension.getURL('test/data/journalArticle-single.html');
+			let url = 'http://zotero-static.s3.amazonaws.com/test.html';
 			let [content, location] = yield tab.run(function(url) {
-				var deferred = Zotero.Promise.defer();
-				sinon.stub(Zotero.HTTP, 'isSameOrigin').returns(false);
-				Zotero.HTTP.processDocuments(url, function(doc) {
-					try {
-						let content = doc.querySelector('.csl-entry').innerText;
-						let location = doc.location.href;
-						deferred.resolve([content, location])
-					} catch (e) {
-						deferred.reject(e);
+				return Zotero.HTTP.processDocuments(
+					url,
+					function (doc) {
+						var content = doc.querySelector('.csl-entry').innerText;
+						var location = doc.location.href;
+						return [content, location];
 					}
-				});
-				
-				return deferred.promise.then(response => {Zotero.HTTP.isSameOrigin.restore(); return response});
+				)
+				.then(vals => vals[0]);
 			}, url);
 			
 			assert.include(content, 'Rosenzweig');
@@ -98,5 +92,20 @@ describe("HTTP", function() {
 				assert.isTrue(hasContentType);
 			}));
 		});
+	});
+	
+	describe("#wrapDocument()", function () {
+		it("should allow document itself to be passed to document.evaluate()", Promise.coroutine(function* () {
+			var content = yield background(function (url) {
+				var url = "https://zotero-static.s3.amazonaws.com/test.html?t";
+				return Zotero.HTTP.request("GET", url, { responseType: 'document' })
+				.then(function (xmlhttp) {
+					var doc = Zotero.HTTP.wrapDocument(xmlhttp.response, url);
+					var div = doc.evaluate('//div', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
+					return div.textContent;
+				});
+			});
+			assert.include(content, 'Rosenzweig');
+		}));
 	});
 });
