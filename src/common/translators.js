dstillman/zotero-connectors

commit 0000fcdca40e517fa6c99ba975b336073e64442f

Refactor Connector.callMethod to use Promises instead of callbacks

src/common/translators.js
@@ -314,12 +314,10 @@ Zotero.Translators.CodeGetter = function(translators, debugMode) {
 Zotero.Translators.CodeGetter.prototype.getCodeFor = Zotero.Promise.method(function(i) {
 	let translator = this._translators[i];
 	// retrieve code if no code and translator is supported locally
-	if((translator.runMode === Zotero.Translator.RUN_MODE_IN_BROWSER && !translator.hasOwnProperty("code"))
-			// or if debug mode is enabled (even if unsupported locally)
-			|| (this._debugMode && (!translator.hasOwnProperty("code")
+	if (translator.runMode === Zotero.Translator.RUN_MODE_IN_BROWSER
 			// or if in debug mode and the code we have came from the repo (which doesn't
 			// include test cases)
-			|| (Zotero.Repo && translator.codeSource === Zotero.Repo.SOURCE_REPO)))) {
+			|| (this._debugMode && translator.codeSource === Zotero.Repo.SOURCE_REPO)) {
 		// get code
 		return translator.getCode().catch((e) => Zotero.debug(`Failed to retrieve code for ${translator.translatorID}`));
 	}

src/browserExt/background.js
@@ -62,33 +62,35 @@ Zotero.Connector_Browser = new function() {
 	 * Called to display select items dialog
 	 */
 	this.onSelect = function(items, callback, tab) {
-		chrome.windows.get(tab.windowId, null, function (win) {
-			var width = 600;
-			var height = 325;
-			var left = Math.floor(win.left + (win.width / 2) - (width / 2));
-			var top = Math.floor(win.top + (win.height / 2) - (height / 2));
-			
-			chrome.windows.create(
-				{
-					url: chrome.extension.getURL("itemSelector/itemSelector.html")
-						+ "#" + encodeURIComponent(JSON.stringify([tab.id, items]))
-						// Remove once https://bugzilla.mozilla.org/show_bug.cgi?id=719905 is fixed
-						.replace(/%3A/g, 'ZOTEROCOLON'),
-					height: height,
-					width: width,
-					top: top,
-					left: left,
-					type: 'popup'
-				},
-				function (win) {
-					// Fix positioning in Chrome when window is on second monitor
-					// https://bugs.chromium.org/p/chromium/issues/detail?id=137681
-					if (Zotero.isChrome && win.left < left) {
-						chrome.windows.update(win.id, { left: left });
+		return new Zotero.Promise(function(resolve) {
+			chrome.windows.get(tab.windowId, null, function (win) {
+				var width = 600;
+				var height = 325;
+				var left = Math.floor(win.left + (win.width / 2) - (width / 2));
+				var top = Math.floor(win.top + (win.height / 2) - (height / 2));
+				
+				chrome.windows.create(
+					{
+						url: chrome.extension.getURL("itemSelector/itemSelector.html")
+							+ "#" + encodeURIComponent(JSON.stringify([tab.id, items]))
+							// Remove once https://bugzilla.mozilla.org/show_bug.cgi?id=719905 is fixed
+							.replace(/%3A/g, 'ZOTEROCOLON'),
+						height: height,
+						width: width,
+						top: top,
+						left: left,
+						type: 'popup'
+					},
+					function (win) {
+						// Fix positioning in Chrome when window is on second monitor
+						// https://bugs.chromium.org/p/chromium/issues/detail?id=137681
+						if (Zotero.isChrome && win.left < left) {
+							chrome.windows.update(win.id, { left: left });
+						}
+						_tabInfo[tab.id].selectCallback = resolve;
 					}
-					_tabInfo[tab.id].selectCallback = callback;
-				}
-			);
+				);
+			});
 		});
 	}
 	

src/browserExt/contentTypeHandler.js
@@ -170,30 +170,22 @@ Zotero.ContentTypeHandler = {
 					if (type == 'csl') {
 						options.method = 'installStyle';
 						options.queryString = 'origin=' + encodeURIComponent(details.url);
-						Zotero.Connector.callMethod(options, this.response, function(result, status, response) {
-							if (result) {
-								Zotero.Messaging.sendMessage('progressWindow.itemProgress',
-									[chrome.extension.getURL('images/csl-style.png'), result.name, null, 100], tab);
-							} else if (status == 404) {
+						return Zotero.Connector.callMethod(options, this.response).then(function(result) {
+							Zotero.Messaging.sendMessage('progressWindow.itemProgress',
+								[chrome.extension.getURL('images/csl-style.png'), result.name, null, 100], tab);
+							return Zotero.Messaging.sendMessage('progressWindow.done', [true], tab);
+						}, function(e) {
+							if (e.status == 404) {
 								return Zotero.Messaging.sendMessage('progressWindow.done',
 									[false, 'upgradeClient'], tab);
 							} else {
 								return Zotero.Messaging.sendMessage('progressWindow.done',
 									[false, 'clientRequired'], tab);
 							}
-							Zotero.Messaging.sendMessage('progressWindow.done', [true], tab);
 						});
 					} else {
 						options.method = 'import';
-						Zotero.Connector.callMethod(options, this.response, function(result, status) {
-							if (status == 404) {
-								return Zotero.Messaging.sendMessage('progressWindow.done',
-									[false, 'upgradeClient'], tab);
-							}
-							if (status < 200 || status > 400) {
-								return Zotero.Messaging.sendMessage('progressWindow.done',
-									[false, 'clientRequired'], tab);
-							}
+						return Zotero.Connector.callMethod(options, this.response).then(function(result) {
 							Zotero.Messaging.sendMessage('progressWindow.show', 
 								`Imported ${result.length} item` + (result.length > 1 ? 's' : ''), tab);
 							for (let i = 0; i < result.length && i < 20; i++) {
@@ -202,6 +194,15 @@ Zotero.ContentTypeHandler = {
 									[Zotero.ItemTypes.getImageSrc(item.itemType), item.title, null, 100], tab);
 							}
 							Zotero.Messaging.sendMessage('progressWindow.done', [true], tab);
+						}, function(e) {
+							if (e.status == 404) {
+								return Zotero.Messaging.sendMessage('progressWindow.done',
+									[false, 'upgradeClient'], tab);
+							}
+							else if (e.status < 200 || status >= 400) {
+								return Zotero.Messaging.sendMessage('progressWindow.done',
+									[false, 'clientRequired'], tab);
+							}
 						});
 					}
 				};

src/browserExt/messaging_inject.js
@@ -72,15 +72,18 @@ Zotero.Messaging = new function() {
 						// send message
 						return new Zotero.Promise(function(resolve, reject) {
 							chrome.runtime.sendMessage([messageName, newArgs], function(response) {
+								if (response && response[0] == 'error') {
+									return reject(response[1]);
+								}
 								try {
 									if (messageConfig.inject && messageConfig.inject.postReceive) {
 										response = messageConfig.inject.postReceive.apply(null, response);
 									}
 									if (callbackArg !== null) callback.apply(null, response);
-									resolve.apply(null, response);
+									return resolve.apply(null, response);
 								} catch(e) {
 									Zotero.logError(e);
-									reject(e);
+									return reject(e);
 								}
 							});
 						});

src/common/connector.js
@@ -129,9 +129,7 @@ Zotero.Connector = new function() {
 				fail();
 			}
 		} else {
-			Zotero.Connector.callMethod("ping", {}, function(status) {
-				deferred.resolve(status !== false);
-			});
+			return Zotero.Connector.ping("ping", {});
 		}
 		return deferred.promise;
 	});
@@ -144,17 +142,14 @@ Zotero.Connector = new function() {
 	}
 	
 	this.ping = function(payload={}) {
-		var deferred = Zotero.Promise.defer();
-		Zotero.Connector.callMethod("ping", payload, function(response, status) {
+		return Zotero.Connector.callMethod("ping", payload).then(function(response) {
 			if (response && 'prefs' in response) {
 				Zotero.Connector.shouldReportActiveURL = !!response.prefs.reportActiveURL;
 				Zotero.Connector.automaticSnapshots = !!response.prefs.automaticSnapshots;
 			}
 			
-			if (response === false) return deferred.reject(status);
-			return deferred.resolve(response);
+			return response;
 		});
-		return deferred.promise;
 	}
 	
 	/**
@@ -168,11 +163,11 @@ Zotero.Connector = new function() {
 	 * @param {Object} data - RPC data to POST. If null or undefined, a GET request is sent.
 	 * @param {Function} callback - Function to be called when requests complete.
 	 */
-	this.callMethod = function(options, data, callback, tab) {
+	this.callMethod = Zotero.Promise.method(function(options, data, tab) {
 		// Don't bother trying if not online in bookmarklet
-		if(Zotero.isBookmarklet && this.isOnline === false) {
-			callback(false, 0);
-			return;
+		var deferred = Zotero.Promise.defer();
+		if (Zotero.isBookmarklet && this.isOnline === false) {
+			throw new Zotero.CommunicationError("Zotero Offline", 0);
 		}
 		if (typeof options == 'string') {
 			options = {method: options};
@@ -185,6 +180,7 @@ Zotero.Connector = new function() {
 			}, options.headers || {});
 		var queryString = options.queryString ? ("?" + options.queryString) : "";
 		
+		var deferred = Zotero.Promise.defer();
 		var newCallback = function(req) {
 			try {
 				var isOnline = req.status !== 0 && req.status !== 403 && req.status !== 412;
@@ -205,7 +201,7 @@ Zotero.Connector = new function() {
 				}
 				if(req.status == 0 || req.status >= 400) {
 					Zotero.debug("Connector: Method "+method+" failed with status "+req.status);
-					if(callback) callback(false, req.status, val);
+					deferred.reject(new Zotero.Connector.CommunicationError('Method failed', req.status, val));
 					
 					// Check for incompatible version
 					if(req.status === 412) {
@@ -218,11 +214,11 @@ Zotero.Connector = new function() {
 					}
 				} else {
 					Zotero.debug("Connector: Method "+method+" succeeded");
-					if(callback) callback(val, req.status);
+					deferred.resolve(val);
 				}
 			} catch(e) {
 				Zotero.logError(e);
-				return;
+				deferred.reject(new Zotero.Connector.CommunicationError(e.message, 0));
 			}
 		};
 		
@@ -234,7 +230,7 @@ Zotero.Connector = new function() {
 					[requestID, method, JSON.stringify(data)]]), `${Zotero.Prefs.get('connector.url')}/connector/ieHack`);
 			} else {
 				Zotero.debug("Connector: No iframe target; not sending to Standalone");
-				callback(false, 0);
+				throw new Zotero.Connector.CommunicationError("No iframe target; not sending to Standalone", 0);
 			}
 		} else {							// Other browsers can use plain doPost
 			var uri = Zotero.Prefs.get('connector.url') + "connector/" + method + queryString;
@@ -247,7 +243,8 @@ Zotero.Connector = new function() {
 				Zotero.HTTP.doPost(uri, data, newCallback, headers);
 			}
 		}
-	},
+		return deferred.promise;
+	}),
 	
 	/**
 	 * Adds detailed cookies to the data before sending "saveItems" request to
@@ -255,12 +252,12 @@ Zotero.Connector = new function() {
 	 *
 	 * @param {String|Object} options. See documentation above
 	 * @param	{Object} data RPC data. See documentation above.
-	 * @param	{Function} callback Function to be called when requests complete.
 	 */
-	this.callMethodWithCookies = function(options, data, callback, tab) {
-		if(Zotero.isBrowserExt && !Zotero.isBookmarklet) {
-			var self = this;
-			chrome.cookies.getAll({url: tab.url}, function(cookies) {
+	this.callMethodWithCookies = function(options, data, tab) {
+		if (Zotero.isBrowserExt && !Zotero.isBookmarklet) {
+			return new Zotero.Promise(function(resolve) {
+				chrome.cookies.getAll({url: tab.url}, resolve);
+			}).then(function(cookies) {
 				var cookieHeader = '';
 				for(var i=0, n=cookies.length; i<n; i++) {
 					cookieHeader += '\n' + cookies[i].name + '=' + cookies[i].value
@@ -278,14 +275,21 @@ Zotero.Connector = new function() {
 				// Cookie URI needed to set up the cookie sandbox on standalone
 				data.uri = tab.url;
 				
-				self.callMethod(options, data, callback, tab);
-			});
-			return;
+				return this.callMethod(options, data, tab);
+			}.bind(this));
 		}
 		
-		this.callMethod(options, data, callback, tab);
+		return this.callMethod(options, data, tab);
 	}
 }
+
+Zotero.Connector.CommunicationError = function (message, status=0, value='') {
+    this.name = 'Connector Communication Error';
+    this.message = message;
+    this.status = status;
+    this.value = value;
+}
+Zotero.Connector.CommunicationError.prototype = new Error;
 
 Zotero.Connector_Debug = new function() {
 	/**

src/common/inject/inject.jsx
@@ -46,17 +46,16 @@ if(isTopWindow) {
 		if (headline) {
 			return Zotero.ProgressWindow.changeHeadline(headline);
 		}
-		Zotero.Connector.callMethod("getSelectedCollection", {}, function(response, status) {
-			if (status !== 200) {
-				Zotero.ProgressWindow.changeHeadline("Saving to zotero.org");
+		return Zotero.Connector.callMethod("getSelectedCollection", {}).then(function(response, status) {
+			Zotero.ProgressWindow.changeHeadline("Saving to ",
+				response.id ? "treesource-collection.png" : "treesource-library.png",
+				response.name+"\u2026");
+		}, function(e) {
+			if (e.value && e.value.libraryEditable === false) {
+				new Zotero.ProgressWindow.ErrorMessage("collectionNotEditable");
+				Zotero.ProgressWindow.startCloseTimer(8000);
 			} else {
-				if (response.libraryEditable === false) {
-					new Zotero.ProgressWindow.ErrorMessage("collectionNotEditable");
-					Zotero.ProgressWindow.startCloseTimer(8000);
-				}
-				Zotero.ProgressWindow.changeHeadline("Saving to ",
-					response.id ? "treesource-collection.png" : "treesource-library.png",
-					response.name+"\u2026");
+				Zotero.ProgressWindow.changeHeadline("Saving to zotero.org");
 			}
 		});
 	});
@@ -144,7 +143,7 @@ Zotero.Inject = new function() {
 					Zotero.Connector_Browser.onTranslators(translators, instanceID, document.contentType);
 				});
 				_translate.setHandler("select", function(obj, items, callback) {
-					Zotero.Connector_Browser.onSelect(items, function(returnItems) {
+					Zotero.Connector_Browser.onSelect(items).then(function(returnItems) {
 						// if no items selected, close save dialog immediately
 						if(!returnItems || Zotero.Utilities.isEmpty(returnItems)) {
 							Zotero.Messaging.sendMessage("progressWindow.close", null);
@@ -381,7 +380,8 @@ Zotero.Inject = new function() {
 	
 	this.saveAsWebpage = function (args) {
 		var title = args[0], withSnapshot = args[1];
-		Zotero.Inject.checkActionToServer().then(function(result) {
+		var progress;
+		return Zotero.Inject.checkActionToServer().then(function(result) {
 			if (!result) return;
 			
 			var data = {
@@ -398,35 +398,36 @@ Zotero.Inject = new function() {
 				var image = "webpage";
 			}
 			
-			var progress = new Zotero.ProgressWindow.ItemProgress(
+			progress = new Zotero.ProgressWindow.ItemProgress(
 				Zotero.ItemTypes.getImageSrc(image), title || document.title
 			);
-			Zotero.Connector.callMethodWithCookies("saveSnapshot", data,
-				function(returnValue, status) {
-					if (returnValue === false) {
-						// Client unavailable
-						if (status === 0) {
-							// Attempt saving to server if not pdf
-							if (document.contentType != 'application/pdf') {
-								Zotero.ProgressWindow.changeHeadline('Saving to zotero.org');
-								let itemSaver = new Zotero.Translate.ItemSaver({});
-								itemSaver.saveAsWebpage().then(function(items) {
-									if (items.length) progress.setProgress(100);
-								});
-							} else {
-								new Zotero.ProgressWindow.ErrorMessage("clientRequired");
-							}
-						} else {
-							new Zotero.ProgressWindow.ErrorMessage("unexpectedError");
-						}
-						Zotero.ProgressWindow.startCloseTimer(8000);
-					} else {
-						progress.setProgress(100);
-						Zotero.ProgressWindow.startCloseTimer(2500);
-					}
+			return Zotero.Connector.callMethodWithCookies("saveSnapshot", data)
+		}.bind(this)).then(function(result) {
+			if (progress) {
+				progress.setProgress(100);
+				Zotero.ProgressWindow.startCloseTimer(2500);
+			}
+			return result;
+		}.bind(this), function(e) {
+			var err;
+			// Client unavailable
+			if (e.status === 0) {
+				// Attempt saving to server if not pdf
+				if (document.contentType != 'application/pdf') {
+					Zotero.ProgressWindow.changeHeadline('Saving to zotero.org');
+					let itemSaver = new Zotero.Translate.ItemSaver({});
+					return itemSaver.saveAsWebpage().then(function(items) {
+						if (items.length) progress.setProgress(100);
+					});
+				} else {
+					err = new Zotero.ProgressWindow.ErrorMessage("clientRequired");
 				}
-			);
-		}.bind(this))
+			} else {
+				err = new Zotero.ProgressWindow.ErrorMessage("unexpectedError");
+			}
+			Zotero.ProgressWindow.startCloseTimer(8000);	
+			if (err) throw err;
+		}.bind(this));
 	};
 };
 
@@ -437,7 +438,9 @@ try {
 } catch(e) {}
 
 // don't try to scrape on hidden frames
-if(!isHiddenIFrame && (window.location.protocol === "http:" || window.location.protocol === "https:")) {
+let isWeb = window.location.protocol === "http:" || window.location.protocol === "https:";
+let isTestPage = window.location.protocol.includes('-extension:') && window.location.href.includes('/test/');
+if(!isHiddenIFrame && (isWeb || isTestPage)) {
 	var doInject = function () {
 		// add listener for translate message from extension
 		Zotero.Messaging.addMessageListener("translate", function(data) {

src/common/messaging.js
@@ -100,12 +100,18 @@ Zotero.Messaging = new function() {
 			
 			var fn = Zotero[messageParts[0]][messageParts[1]];
 			if(!fn) throw new Error("Zotero."+messageParts[0]+"."+messageParts[1]+" is not defined");
-			var maybePromise = fn.apply(Zotero[messageParts[0]], args);
+			try {
+				var maybePromise = fn.apply(Zotero[messageParts[0]], args);
+			} catch (e) {
+				if (shouldRespond) {
+					args[callbackArg](['error', e]);
+				}
+			}
 			// If you thought the message passing system wasn't complicated enough as it were,
 			// now background page functions can return promises too 👍
 			if (shouldRespond) {
 				if (maybePromise && maybePromise.then) {
-					maybePromise.then(args[callbackArg]);
+					maybePromise.then(args[callbackArg]).catch(e => args[callbackArg](['error', e]));
 				} else {
 					args[callbackArg](maybePromise);
 				}

src/common/promise.js
@@ -30,7 +30,16 @@
 
 Zotero.Promise.method = function(fn) {
 	return function() {
-		return Promise.resolve(fn.apply(this, arguments));
+		try {
+			var val = fn.apply(this, arguments);
+			if (val && val.then) {
+				return val;
+			} else {
+				return Promise.resolve(val);
+			}
+		} catch (e) {
+			return Promise.reject(e);
+		}
 	}
 };
 

commit d3a4a9c331892da901d50a62c64ccd6a21d19147

Fix "Zotero.Repo is undefined" error in translation-server
Regression from 0000fcdc

src/common/translators.js
@@ -331,7 +331,7 @@ Zotero.Translators.CodeGetter.prototype.getCodeFor = Zotero.Promise.method(funct
 	if (translator.runMode === Zotero.Translator.RUN_MODE_IN_BROWSER
 			// or if in debug mode and the code we have came from the repo (which doesn't
 			// include test cases)
-			|| (this._debugMode && translator.codeSource === Zotero.Repo.SOURCE_REPO)) {
+			|| (this._debugMode && Zotero.Repo && translator.codeSource === Zotero.Repo.SOURCE_REPO)) {
 		// get code
 		return translator.getCode().catch((e) => Zotero.debug(`Failed to retrieve code for ${translator.translatorID}`));
 	}